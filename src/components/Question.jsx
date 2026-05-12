de5dfd87b8e4b4f0a4945de83eca327de1001779
ANGELA-NYUTU <angie.nimo254@gmail.com>
2026-05-12 17:37:27 +0300

    added event listener to show ans clicked

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 function Question() {
+  function handleClick(number){
+    console.log("clicked answer " + number)
+  }
   return (
     <>
         <h1> QUESTION HERE </h1>
-        <button> Answer 1</button>
-        <button> Answer 2</button>
-        <button> Answer 3</button>
-        <button> Answer 4</button>
+        <button onClick={() => handleClick(1)}> Answer 1</button>
+        <button onClick={() => handleClick(2)}> Answer 2</button>
+        <button onClick={() => handleClick(3)}> Answer 3</button>
+        <button onClick={() => handleClick(4)}> Answer 4</button>
     </>
   );
 }
